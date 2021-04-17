bead1b63f60a2c9d0fd60303f1e3c920f1151af8
ppidcard <tvxqlikei@live.cn>
2021-04-17 19:06:46 +1000

    Done with lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+const returnFirstTwoDrivers = function(arr){
+    return arr.slice(0,2);
+}
+
+const returnLastTwoDrivers = function(arr){
+    return arr.slice(2,4);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+const createFareMultiplier = function(multiplier){
+  return function(fare){
+    return multiplier * fare;
+  };
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function(arr=drivers, returnFirstTwoDrivers){
+  return returnFirstTwoDrivers(arr);
+}
+
